ae015880c48b70a6c827cc04ec58af49cce5b09e
alexandrossilva <alexandrossilva@gmail.com>
2024-11-25 05:37:58 -0300

    Codificações da aula do dia 25/11/2024

---
+++ aula-2024-11-25/pagina03.js
@@ -0,0 +1,23 @@
+function adicionarSol() {
+    let p = document.querySelector("p");
+
+    let img = document.createElement("img");
+    img.setAttribute("src", "imagens/sol.png");
+
+    p.appendChild(img);
+}
+
+function adicionarLua() {
+    let p = document.querySelector("p");
+
+    let img = document.createElement("img");
+    img.setAttribute("src", "imagens/lua.png");
+
+    p.appendChild(img);
+}
+
+let btnSol = document.getElementById("add-sol");
+let btnLua = document.getElementById("add-lua");
+
+btnSol.addEventListener("click", adicionarSol);
+btnLua.addEventListener("click", adicionarLua);
