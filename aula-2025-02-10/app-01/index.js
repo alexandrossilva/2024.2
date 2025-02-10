fe6e40c11b6c757f0a1cc2a50b95f714335b9479
alexandrossilva <alexandrossilva@gmail.com>
2025-02-10 20:12:20 -0300

    Aula do dia 10/02/2025

--- aula-2025-02-10/app-01/index.js
+++ aula-2025-02-10/app-01/index.js
@@ -13,4 +13,21 @@ app.get("/soma", (req, resp) => {
     resp.end("Soma: " + soma);
 });
 
+app.get("/operacao", (req, resp) => {
+    let n1 = req.query["n1"];
+    let n2 = req.query["n2"];
+    let op = req.query["op"];
+
+    let res;
+
+    switch(op) {
+        case "som": res = parseInt(n1) + parseInt(n2); break;
+        case "subt": res = parseInt(n1) - parseInt(n2); break;
+        case "mult": res = parseInt(n1) * parseInt(n2); break;
+        case "div": res = parseInt(n1) / parseInt(n2); break;
+    }
+
+    resp.end("Resultado: " + res);
+});
+
 app.listen(3000);
